fafbf245d9b6dace4a901e7a7143c0752c00ce67
guxiaoyu <1406803165@qq.com>
2019-01-30 13:42:30 +0800

    MD5

--- app/src/main/java/com/zrt/rccfgwd/utils/EncryptionTools.kt
+++ app/src/main/java/com/zrt/rccfgwd/utils/EncryptionTools.kt
@@ -3,6 +3,7 @@ package com.zrt.rccfgwd.utils
 import android.annotation.SuppressLint
 import java.text.SimpleDateFormat
 import java.util.*
+import kotlin.collections.ArrayList
 
 /**
  * Created by Administrator on 2019/1/29.
@@ -16,7 +17,8 @@ object EncryptionTools {
         TIMESTAMP = getDateToString("yyyyMMddHHmmss")
         NONCE = Random().nextInt(10000).toString()
         var stitching = Token + TIMESTAMP + NONCE
-
+        var str = sort(stitching) + Config.key
+//        SIGNATURE =
     }
 
     fun getDateToString(pattern:String):String{
@@ -25,7 +27,16 @@ object EncryptionTools {
         return format.format(date)
     }
 
-//    fun sort(str:String):String{
-//        str.to
-//    }
+    fun sort(str:String):String{
+        val ch = str.toCharArray()
+        val list = ArrayList<Char>()
+        ch.indices.mapTo(list) { ch[it] }
+        Collections.sort(list)
+        val sBuffer = StringBuffer()
+        for (i in ch.indices) {
+            sBuffer.append(list[i])
+        }
+
+        return sBuffer.toString()
+    }
 }
